ff7cc59f891efda19ea84fc8e5fa68038e42cdf9
main <feelkbt@gmail.com>
2023-11-13 17:06:39 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-img height="300" src="@/assets/iszion_logo.png" />
+
+      <h1 class="pt-15 text-h2 font-weight-bold text-light-blue">Coming soon</h1>
+
+      <div class="py-14" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-icon
+            icon="mdi-view-dashboard"
+            size="large"
+            start
+          />
+          맞춤형 전산업무 개발(제조/도소매/유통/공사/수출입/도서출판유통 등)
+        </v-col>
+        <v-col cols="auto">
+          <v-icon
+            icon="mdi-refresh"
+            size="large"
+            start
+          />
+          Web ERP & Acu-Cobol(Migration)
+        </v-col>
+      </v-row>
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-icon
+            icon="mdi-database"
+            size="large"
+            start
+          />
+          Migration Acu-Cobol ISAM Data to Database
+        </v-col>
+
+
+      <v-col cols="auto">
+          <v-icon
+            icon="mdi-phone"
+            size="large"
+            start
+          />
+          Maintenance Support (유지보수)
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
